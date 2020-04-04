7536e72dcd39d2e8fc6da295ca10bb5a5376474d
bryanvargas <bryanprsvar@gmail.com>
2020-04-04 12:51:37 -0300

    Condiciones

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+const Idioma = ({traduccion}) =>{
+  if(traduccion == 'es') return <h1>Hola</h1>;
+  if(traduccion == 'en') return <h1>Hello</h1>;
+}
+
 const Saludo = () =>{
   const nombre = 'Shingo';
 
-  return <p> { ` Hola Mundo ${nombre} ` }</p>
+  return <p> <Idioma traduccion="es" /> { ` ${nombre}` }</p>
 }
 
 const App = () =>{
