45cfe62abcb3dde635d65a11d38549715af0976c
bryanvargas <bryanprsvar@gmail.com>
2020-04-04 13:03:37 -0300

    operador ternario

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const Idioma = ({traduccion}) =>{
-  if(traduccion == 'es') return <span>Hola</span>;
-  if(traduccion == 'en') return <span>Hello</span>;
-}
+//const Idioma = ({traduccion}) =>{
+//  if(traduccion == 'es') return <span>Hola</span>;
+//  if(traduccion == 'en') return <span>Hello</span>;
+// }
 
 const Saludo = () =>{
   const nombre = 'Shingo';
+  const idioma = 'en';
+/*  return(
+    <div>
+      {
+        idioma === "es" && <p>Hola</p> 
+      }
+      {
+        idioma === 'en' && <p>Hola</p> 
+      }
+    </div>
+  ) */
+  return(
+    <div>
+      {
+        idioma === 'en' ? <p>Hello</p> : <p>hola</p>
+      }
+    </div>
+  )
 
-  return <p> <Idioma traduccion="es" /> { ` ${nombre}` }</p>
+ // return <p> <Idioma traduccion="es" /> { ` ${nombre}` }</p>
 }
 
 const App = () =>{
